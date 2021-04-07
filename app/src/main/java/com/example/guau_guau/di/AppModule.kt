0b11251b3a9f3d4f5aa614e8d65d36464e0fb16b
Luis Cruz <luisccc99@gmail.com>
2021-04-07 16:18:45 -0600

    adding annotations parcelize, and singleton component (AppModule)

--- app/src/main/java/com/example/guau_guau/di/AppModule.kt
+++ app/src/main/java/com/example/guau_guau/di/AppModule.kt
@@ -3,11 +3,13 @@ package com.example.guau_guau.di
 import com.example.guau_guau.api.GuauguauApi
 import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 @Module
-@InstallIn(ApplicationComponent::class)
+@InstallIn(SingletonComponent::class)
 object AppModule {
 
     @Provides
