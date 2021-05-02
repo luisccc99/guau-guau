39826f713fa5ab1f69d7b789212e910b510cb640
Miguel Valdez <56283510+MiquelVald@users.noreply.github.com>
2021-05-02 01:19:25 -0600

    Cute comments

--- app/src/main/java/com/example/guau_guau/data/GuauguauComment.kt
+++ app/src/main/java/com/example/guau_guau/data/GuauguauComment.kt
@@ -2,11 +2,15 @@ package com.example.guau_guau.data
 
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
+import java.util.*
 
 @Parcelize
 data class GuauguauComment(
     val body: String,
-    val created_at: String,
+    val created_at: Date,
+    val name: String,
+    val lastname: String,
+    val user_photo: String,
     val id: String,
     val post_id: String,
     val updated_at: String,
